fd638588f306d74a21793bd18f9d8c9ffeac66b0
CarlosLup <moralesyaguilera@gmail.com>
2024-06-01 11:58:53 -0400

    swapi

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
+
+
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { anyfunction } from './api/moviesApi.ts';
+
 </script>
 
 <template>
